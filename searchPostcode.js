3a6b87235e0922f40a5ee9b273006e2ea9a99870
WillFoster95 <will-f@hotmail.co.uk>
2020-12-03 21:11:22 +0000

    Raw forcast data from ClimaCell API

--- searchPostcode.js
+++ searchPostcode.js
@@ -1,13 +1,45 @@
 import apikey from './apikey.js';
+import ccapikey from './apikey.js';
 
 const searchButton = document.querySelector('#searchButton');
 const searchInputField = document.querySelector('#searchInputField');
 const testParagraph = document.querySelector('#testParagraph');
+const latValue = document.querySelector("#searchLat");
+const lonValue = document.querySelector("#searchLong");
+
+//urls for climacell
+const url_CCforcast = 'https://api.climacell.co/v3/weather/forecast/daily';
+const url_CClat = '?lat=';
+const url_CClon = '&lon=';
+const url_CCdefaultTest = '&unit_system=si&start_time=now&fields=precipitation&apikey='
+
+const CCAPI_key = ccapikey;
+
+//urls for DataPoint
 const url_sitelist = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=';
 const url_forcasts = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/';
 
 const api_key = apikey;
 
+//for climacell API using long and lat
+const searchCoordinatesButtonPressed = async () => {
+  try{        
+    const response = await fetch(url_CCforcast + url_CClat + latValue.value + url_CClon + lonValue.value + url_CCdefaultTest + CCAPI_key);
+    if (response.ok){
+      const jsonResponse = await response.json();
+      testParagraph.innerHTML = JSON.stringify(jsonResponse);
+             
+      //return jsonResponse;
+    }
+    throw new Error('Request failed!')
+  }
+  catch(error){
+    console.log(error);    
+  }
+}
+
+
+//For Data Point API using place name (most of the code below here may not be used as ClimaCell seems better than DataPoint)
 const searchButtonPressed = async () => {
   let rawLocationData = await fetchRawLocationData();
   let locationsArray = rawLocationData.Locations.Location
@@ -23,6 +55,7 @@ const searchButtonPressed = async () => {
   //testParagraph.innerHTML = JSON.stringify(jsonResponse.Locations.Location);
 }
 
+//Returns a site list of all sties avaliable in the DataPoint API
 const fetchRawLocationData = async () => {
     try{
       console.log(url_sitelist + api_key);      
@@ -39,6 +72,7 @@ const fetchRawLocationData = async () => {
     }
 }
 
+//Finds the index of the location from the array of all locations in the DataPoint API
 const findLocationIndex = (locationArray) => {
   for(let i = 0; i<locationArray.length; i++){
     if(locationArray[i].name == searchInputField.value){      
@@ -48,6 +82,7 @@ const findLocationIndex = (locationArray) => {
   return "place not found";
 }
 
+//Returns location forcast by location ID from the DataPoint API
 const fetchLocationForcastsByID = async (locationID) => {
   try{
     //console.log(url_forcasts + locationID + '?res=3hourly&key=' + api_key); 
@@ -65,3 +100,4 @@ const fetchLocationForcastsByID = async (locationID) => {
 }
 
 searchButton.addEventListener('click', searchButtonPressed);
+searchCoordinates.addEventListener('click', searchCoordinatesButtonPressed);
