f74935861409bfa376f52f60153ced4c7df54d79
WillFoster95 <will-f@hotmail.co.uk>
2020-12-14 13:43:43 +0000

    Display data for today and next 5 days

--- searchPostcode.js
+++ searchPostcode.js
@@ -16,25 +16,31 @@ const url_CClon = '&lon=';
 
 //Next 5 days
 const url_CCNext5 = 'https://api.climacell.co/v3/weather/forecast/daily';
-const url_CCdefaultTest = '&unit_system=si&start_time=now&fields=precipitation&apikey='
+const url_CCdefaultTest = '&unit_system=si&start_time=now&fields=precipitation%2Ctemp%2Cwind_speed&apikey=';
 
 //apikey
 const CCAPI_key = ccapikey;
 
 //for climacell API using long and lat
 const searchCoordinatesButtonPressed = async () => {
-  let Next5Data = await fetchNext5DaysForcast();
+  let Next5Data = await fetchNext5DaysForcast(); 
   //let Last5Data = await fetchNext5DaysForcast();
   //testParagraph.innerHTML = JSON.stringify(Next5Data);
 
-  RD1.innerHTML = Next5Data[1].precipitation[0].max.value
-  RD2.innerHTML = Next5Data[2].precipitation[0].max.value
-  RD3.innerHTML = Next5Data[3].precipitation[0].max.value
-  RD4.innerHTML = Next5Data[4].precipitation[0].max.value
-  RD5.innerHTML = Next5Data[5].precipitation[0].max.value
+  RD0.innerHTML = Next5Data[0].precipitation[0].max.value;
+  
+  TD0.innerHTML = Next5Data[0].temp[0].min.value + " - " + Next5Data[0].temp[1].max.value;
+  WD0.innerHTML = Next5Data[0].wind_speed[0].min.value + " - " + Next5Data[0].wind_speed[1].max.value;
 
+  RD1.innerHTML = Next5Data[1].precipitation[0].max.value;
+  RD4.innerHTML = Next5Data[4].precipitation[0].max.value;
+  RD2.innerHTML = Next5Data[2].precipitation[0].max.value;
+  RD3.innerHTML = Next5Data[3].precipitation[0].max.value;
+  RD5.innerHTML = Next5Data[5].precipitation[0].max.value;
 }
 
+//The bandwidth used by this function can be reduced by 
+//specifying an end time and not calling temp and wind speed when not required
 const fetchNext5DaysForcast = async () => {
   try{        
     const response = await fetch(url_CCNext5 + url_CClat + searchLat.value + url_CClon + searchLong.value + url_CCdefaultTest + CCAPI_key);
