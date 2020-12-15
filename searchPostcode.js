eb48f52c012a874fcc5052b0a70532e4b3700111
WillFoster95 <will-f@hotmail.co.uk>
2020-12-15 16:31:36 +0000

    Changed from mm per hour to total rainfall per day

--- searchPostcode.js
+++ searchPostcode.js
@@ -16,7 +16,7 @@ const url_CClon = '&lon=';
 
 //Next 5 days
 const url_CCNext5 = 'https://api.climacell.co/v3/weather/forecast/daily';
-const url_CCdefaultTest = '&unit_system=si&start_time=now&fields=precipitation%2Ctemp%2Cwind_speed&apikey=';
+const url_CCdefaultTest = '&unit_system=si&start_time=now&fields=precipitation_accumulation%2Ctemp%2Cwind_speed&apikey=';
 
 //apikey
 const CCAPI_key = ccapikey;
@@ -27,16 +27,17 @@ const searchCoordinatesButtonPressed = async () => {
   //let Last5Data = await fetchNext5DaysForcast();
   //testParagraph.innerHTML = JSON.stringify(Next5Data);
 
-  RD0.innerHTML = Next5Data[0].precipitation[0].max.value;
+  RD0.innerHTML = Math.round(Next5Data[0].precipitation_accumulation.value *  100)/100;
   
   TD0.innerHTML = Next5Data[0].temp[0].min.value + " - " + Next5Data[0].temp[1].max.value;
   WD0.innerHTML = Next5Data[0].wind_speed[0].min.value + " - " + Next5Data[0].wind_speed[1].max.value;
 
-  RD1.innerHTML = Next5Data[1].precipitation[0].max.value;
-  RD4.innerHTML = Next5Data[4].precipitation[0].max.value;
-  RD2.innerHTML = Next5Data[2].precipitation[0].max.value;
-  RD3.innerHTML = Next5Data[3].precipitation[0].max.value;
-  RD5.innerHTML = Next5Data[5].precipitation[0].max.value;
+  RD1.innerHTML = Math.round(Next5Data[1].precipitation_accumulation.value *  100)/100;
+  
+  RD2.innerHTML = Math.round(Next5Data[2].precipitation_accumulation.value *  100)/100;
+  RD3.innerHTML = Math.round(Next5Data[3].precipitation_accumulation.value *  100)/100;
+  RD4.innerHTML = Math.round(Next5Data[4].precipitation_accumulation.value *  100)/100;
+  RD5.innerHTML = Math.round(Next5Data[5].precipitation_accumulation.value *  100)/100;
 }
 
 //The bandwidth used by this function can be reduced by 
