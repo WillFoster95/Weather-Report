90fbe286a16b94829f9e32ee4f4d331ec19df985
WillFoster95 <will-f@hotmail.co.uk>
2020-12-16 11:42:14 +0000

    Symbols and exe timer

--- searchPostcode.js
+++ searchPostcode.js
@@ -15,36 +15,53 @@ const url_CClat = '?lat=';
 const url_CClon = '&lon=';
 
 //Next 5 days
-const url_CCNext5 = 'https://api.climacell.co/v3/weather/forecast/daily';
-const url_CCdefaultTest = '&unit_system=si&start_time=now&fields=precipitation_accumulation%2Ctemp%2Cwind_speed&apikey=';
+const url_Next5Start = 'https://api.climacell.co/v3/weather/forecast/daily';
+const url_Next5End = '&unit_system=si&start_time=now&fields=precipitation_accumulation%2Ctemp%2Cwind_speed&apikey=';
+
+//Hourly
+const url_HourlyStart = "https://api.climacell.co/v3/weather/forecast/hourly"
+const url_HourlyEnd = "&unit_system=si&start_time=now&fields=precipitation%2Ctemp%2Cwind_speed%2Cwind_gust&apikey="
 
 //apikey
 const CCAPI_key = ccapikey;
 
 //for climacell API using long and lat
 const searchCoordinatesButtonPressed = async () => {
-  let Next5Data = await fetchNext5DaysForcast(); 
-  //let Last5Data = await fetchNext5DaysForcast();
+  var startTime = new Date();
+  let Next5Data = await fetchNext5DaysForcast();          //Could be done in parallel
+  let TodayData = await fetchTodaysForcast();             //to reduce exection time
+  //let Last5Data = await fetchLast5DaysForcast();
   //testParagraph.innerHTML = JSON.stringify(Next5Data);
-
-  RD0.innerHTML = Math.round(Next5Data[0].precipitation_accumulation.value *  100)/100;
   
+  //Today
+  RD0.innerHTML = Math.round(Next5Data[0].precipitation_accumulation.value *  100)/100; 
   TD0.innerHTML = Math.round(Next5Data[0].temp[0].min.value * 10)/10 + " - " + Math.round(Next5Data[0].temp[1].max.value * 10)/10;
   WD0.innerHTML = Math.round(Next5Data[0].wind_speed[0].min.value * 10)/10 + " - " + Math.round(Next5Data[0].wind_speed[1].max.value * 10)/10;
   
-
+  //Next 5 days
   RD1.innerHTML = Math.round(Next5Data[1].precipitation_accumulation.value *  100)/100;
   RD2.innerHTML = Math.round(Next5Data[2].precipitation_accumulation.value *  100)/100;
   RD3.innerHTML = Math.round(Next5Data[3].precipitation_accumulation.value *  100)/100;
   RD4.innerHTML = Math.round(Next5Data[4].precipitation_accumulation.value *  100)/100;
   RD5.innerHTML = Math.round(Next5Data[5].precipitation_accumulation.value *  100)/100;
+
+  //Hourly
+  RH0.innerHTML = Math.round(TodayData[0].precipitation.value *  100)/100;
+  TH0.innerHTML = Math.round(TodayData[0].temp.value * 10)/10;
+  WH0.innerHTML = Math.round(TodayData[0].wind_speed.value *10)/10;
+  WGH0.innerHTML = Math.round(TodayData[0].wind_gust.value *10)/10;
+
+  var endTime = new Date();
+  var exeTime = endTime.getTime() - startTime.getTime();
+  testParagraph.innerHTML = exeTime;
+
 }
 
 //The bandwidth used by this function can be reduced by 
 //specifying an end time and not calling temp and wind speed when not required
 const fetchNext5DaysForcast = async () => {
   try{        
-    const response = await fetch(url_CCNext5 + url_CClat + searchLat.value + url_CClon + searchLong.value + url_CCdefaultTest + CCAPI_key);
+    const response = await fetch(url_Next5Start + url_CClat + searchLat.value + url_CClon + searchLong.value + url_Next5End + CCAPI_key);
     if (response.ok){
       const jsonResponse = await response.json();                 
       return jsonResponse;
@@ -62,7 +79,18 @@ const fetchLast5DaysForcast = async () => {
 
 //For hourly forcast 
 const fetchTodaysForcast = async () => {
-
+  try{        
+    const response = await fetch(url_HourlyStart + url_CClat + searchLat.value + url_CClon + searchLong.value + url_HourlyEnd + CCAPI_key);
+    if (response.ok){
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);                 
+      return jsonResponse;
+    }
+    throw new Error('Request failed!')
+  }
+  catch(error){
+    console.log(error);    
+  }
 }
 
 //urls for DataPoint
