65b37fc9dba6ab50bff699d79d24ec54bc6f9708
WillFoster95 <will-f@hotmail.co.uk>
2021-01-07 11:14:24 +0000

    Revmoved redundent code and added test react element

--- searchPostcode.js
+++ searchPostcode.js
@@ -1,14 +1,5 @@
-import apikey from './apikey.js';
 import ccapikey from './apikey.js';
-
-/*
-const searchButton = document.querySelector('#searchButton');
-const searchCoordinates = document.querySelector('#searchCoordinates');
-const searchInputField = document.querySelector('#searchInputField');
-const testParagraph = document.querySelector('#testParagraph');
-const latValue = document.querySelector("#searchLat");
-const lonValue = document.querySelector("#searchLong");
-*/
+//import React from 'react';
 
 //urls for climacell
 const url_CClat = '?lat=';
@@ -137,74 +128,34 @@ const fetchTodaysForcast = async () => {
   }
 }
 
+//Testing react element
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#test_React_container');
+ReactDOM.render(e(LikeButton), domContainer);
+//end of testing react element
+
+
 searchCoordinates.addEventListener('click', searchCoordinatesButtonPressed);
 
-/*
-//urls for DataPoint
-const url_sitelist = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=';
-const url_forcasts = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/';
-
-const api_key = apikey;
-
-//For Data Point API using place name (most of the code below here may not be used as ClimaCell seems better than DataPoint)
-const searchButtonPressed = async () => {
-  let rawLocationData = await fetchRawLocationData();
-  let locationsArray = rawLocationData.Locations.Location
-  let locationIndex = findLocationIndex(locationsArray);   
-  let locationID = locationsArray[locationIndex].id;
-  let locationForcasts = await fetchLocationForcastsByID(locationID);
-
-  testParagraph.innerHTML = JSON.stringify(locationForcasts);
-
-  //testParagraph.innerHTML = locationID;
-  //console.log(locationIndex);
-  //testParagraph.innerHTML = JSON.stringify(locationsArray[locationIndex]);
-  //testParagraph.innerHTML = JSON.stringify(jsonResponse.Locations.Location);
-}
-
-//Returns a site list of all sties avaliable in the DataPoint API
-const fetchRawLocationData = async () => {
-    try{
-      console.log(url_sitelist + api_key);      
-      const response = await fetch(url_sitelist + api_key);
-      if (response.ok){
-        const jsonResponse = await response.json();
-               
-        return jsonResponse;
-      }
-      throw new Error('Request failed!')
-    }
-    catch(error){
-      console.log(error);    
-    }
-}
-
-//Finds the index of the location from the array of all locations in the DataPoint API
-const findLocationIndex = (locationArray) => {
-  for(let i = 0; i<locationArray.length; i++){
-    if(locationArray[i].name == searchInputField.value){      
-      return i;
-    }
-  }
-  return "place not found";
-}
-
-//Returns location forcast by location ID from the DataPoint API
-const fetchLocationForcastsByID = async (locationID) => {
-  try{
-    //console.log(url_forcasts + locationID + '?res=3hourly&key=' + api_key); 
-    const response = await fetch(url_forcasts + locationID + '?res=3hourly&key=' + api_key);
-    if (response.ok){
-      const jsonResponse = await response.json();
-      return jsonResponse;
-    }
-    throw new Error('Request failed!')
-  }
-  catch(error){
-    console.log(error);    
-  }
-
-}
-
-searchButton.addEventListener('click', searchButtonPressed);
-*/
